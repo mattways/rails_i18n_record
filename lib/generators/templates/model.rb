0b86557a31fb73d3d72193708c0849bfc59eeb5f
Mathías Montossi <mmontossi@museways.com>
2014-06-09 15:31:36 -0300

    Fix gemspec dependencies

--- lib/generators/templates/model.rb
+++ lib/generators/templates/model.rb
@@ -1,9 +1,5 @@
 class <%= "#{class_name}Translation" %> < ActiveRecord::Base
-  <% if Rails::VERSION::MAJOR < 4 %>
-
-    attr_accessible :locale, <%= ":#{singular_table_name}_id" %>, <%= @model ? @model.translatable_attrs.map{ |a| ":#{a}" }.join(', ') : '' %>
-
-  <% end %>
+ 
   belongs_to <%= ":#{table_name}" %>
  
   validates :locale, presence: true
--- test/dummy/app/models/model_translation.rb
+++ test/dummy/app/models/model_translation.rb
@@ -1,6 +1,4 @@
 class ModelTranslation < ActiveRecord::Base
-  
-  attr_accessible :locale, :model_id, :name if Rails::VERSION::MAJOR < 4
  
   belongs_to :models
  
