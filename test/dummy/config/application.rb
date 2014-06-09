46095be2ee0e4074640362f0c1d378adba084db4
Mathías Montossi <mmontossi@museways.com>
2014-06-09 16:37:37 -0300

    Fix test dummy config

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "translatable_records"
+require 'translatable_records'
 
 module Dummy
   class Application < Rails::Application
@@ -18,6 +18,5 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    I18n.enforce_available_locales = false
   end
 end
