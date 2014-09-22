5174afc4c1f0beef8a1bb4d5707d16c4cab7d8f9
Mathías Montossi <mmontossi@museways.com>
2014-09-22 14:40:20 -0300

    Fix gemspec

--- test/generators_test.rb
+++ test/generators_test.rb
@@ -11,7 +11,7 @@ class GeneratorsTest < Rails::Generators::TestCase
   tests TranslationGenerator
   destination File.expand_path('../tmp', File.dirname(__FILE__))
 
-  test 'generate files' do
+  test 'files generation' do
     run_generator %w(model)
     assert_file 'app/models/model_translation.rb'
     assert_migration 'db/migrate/create_model_translations.rb'
